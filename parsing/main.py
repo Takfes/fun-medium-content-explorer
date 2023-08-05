39a57335309059d8c28dd9f6f1e62109c87e013e
tfe <Takfes@users.noreply.github.com>
2023-08-05 13:58:10 +0300

    updates in parsing

--- parsing/main.py
+++ parsing/main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from parsing.config import READ_FOLDER, SAVE_FOLDER
@@ -7,21 +9,25 @@ from parsing.myio import dump_to_path, load_from_path
 
 
 def parse_url(url):
-    pass
+    response = requests.get(url)
+    response.content
+    soup = BeautifulSoup(response.content, "html.parser")
+    return {"title": soup.title.text, "content": " ".join([x.text for x in soup.find_all("p")])}
 
 
 if __name__ == "__main__":
     readpath = Path(READ_FOLDER)
     readfiles = list(readpath.glob("*.json"))
-    for f in readfiles[1]:
-        f = readfiles[1]
+    for f in readfiles:
+        # f = readfiles[1]
         items = load_from_path(f)
         print(f"Parsing : {f.name}")
         saver = []
-        for item in tqdm(items[10:15]):
+        for item in tqdm(items):
+            item = items[100]
             mid, url = item.get("mediumid"), item.get("medium_url")
-            text = parse_url(url)
-            saver.append({mid: text})
+            content = parse_url(url)
+            saver.append({mid: content})
         dump_to_path(saver, f'{SAVE_FOLDER}/{f.name.split("|")[0]}')
 
         items[13].get("medium_url")
