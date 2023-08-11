590156f020e3df352b64fb4ef82526b42969d060
tfe <Takfes@users.noreply.github.com>
2023-08-11 21:12:51 +0300

    update setup + reqs

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.read().split("\n")
 
 setup(
-    name="myscrapers",
-    version="0.0.1",
+    name="chicken",
+    version="0.1.1",
     install_requires=requirements,
     py_modules=["scraping", "crawling", "parsing", "config"],
     entry_points={
